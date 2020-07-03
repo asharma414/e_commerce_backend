5d1e075d1fd91cc89e37f8014e1bbe5338ed96fc
asharma414 <abhi.sharm.414@gmail.com>
2020-07-03 14:01:13 -0400

    show page route

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,7 +1,11 @@
 class ArtifactsController < ApplicationController
 
     def index
-        render json: Artifact.all
+        render json: Artifact.all.to_json(:only => [ :id, :title, :image_url, :list_price, :description, :verification, :century])
+    end
+
+    def show
+        render json: Artifact.find(params[:id]).to_json(:except => [ :created_at, :updated_at ])
     end
 
 end
