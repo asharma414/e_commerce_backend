826666afd53026990411e49663fe47493b67dbd4
saimaja <saimaakhtar@gmail.com>
2020-07-06 12:32:52 -0400

    category route

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,11 +1,18 @@
 class ArtifactsController < ApplicationController
 
     def index
-        render json: Artifact.all.to_json(:only => [ :id, :title, :primary_image, :list_price, :description, :verification, :century])
+        render json: Artifact.all.to_json(:only => [ :id, :title, :primary_image, :list_price, :description, :verification, :century, :classification])
+    end
+
+    def category
+        arr = []
+        params[:category].each {|k, v| arr << v}
+        render json: Artifact.where(category: [Category.where(name: arr)])
     end
 
     def show
         render json: Artifact.find(params[:id]).to_json(:except => [ :created_at, :updated_at ])
     end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get '/artifacts/category', to: 'artifacts#category'
+  
   resources :artifacts
   resources :orders
   resources :users
   resources :categories
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
