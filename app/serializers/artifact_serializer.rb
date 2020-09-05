75566d0641f8ef885ef880dafad7ac825470944b
saimaja <saimaakhtar@gmail.com>
2020-09-05 13:29:05 -0400

    purchase date

--- app/serializers/artifact_serializer.rb
+++ app/serializers/artifact_serializer.rb
@@ -1,3 +1,3 @@
 class ArtifactSerializer < ActiveModel::Serializer
-  attributes :id, :category_id, :technique, :dated, :provenance, :description, :sold, :list_price, :dimensions, :century, :medium, :accession_method, :accession_year, :culture, :artist_name, :artist_role, :artist_birthplace, :artist_lifetime, :title, :verification, :images, :primary_image, :verification_number
+  attributes :id, :updated_at, :category_id, :technique, :dated, :provenance, :description, :sold, :list_price, :dimensions, :century, :medium, :accession_method, :accession_year, :culture, :artist_name, :artist_role, :artist_birthplace, :artist_lifetime, :title, :verification, :images, :primary_image, :verification_number
 end
